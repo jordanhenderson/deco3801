8457e3b6a52d7f1225ddcd2ca92a5c6947439cc9
Jordan Henderson <jordan.henderson@ioflame.com>
2014-08-15 01:37:04 +1000

    Added quick file upload support.

--- submit.php
+++ submit.php
@@ -45,11 +45,56 @@
 	</nav>
 	
 	<div class="container">
+	      
 		<h1>Assignment 99 Submission</h1>
-		<div class="col-lg-12">
-			<h2>Code Upload</h2>
-			<p>Your files have already been uploaded, and are currently awaiting testing.</p>
+		<div class="col-sm-12">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+			Assignment Submission
+			</div>
+			<div class="panel-body">
+			<div class="col-sm-6">
+			<h2>File Submission</h1>
+			<p>Upload your assignment using the file submission method.<br>File types supported: .zip, .rar</p>
+			<form action="upload.php" method="post" enctype="multipart/form-data">
+			<span class="btn btn-default btn-file">
+			    Browse <input type="file" name="file" id="file">
+			</span>
+			<button type="submit" value="Submit" class="btn btn-success">Submit</button>
+			</form>
+			
+			</div>
+			<div class="col-sm-6">
+				<h1>Repository Submission</h1>
+				<p>Upload your assignment using the repository submission method (GIT or SVN).
+				</p>
+				<div class="form-group">
+				<label for="repotype">Repository Type</label>
+				<select class="form-control" name="repotype" id="repotype">
+				    <option value="git">Git</option>
+				    <option value="svn">SVN</option>
+				</select>
+				<div>
+				<div class="form-group">
+					<label for="url">URL:</label>
+					<input class="form-control" type="text" id="url" name="url">
+				</div>
+				<div class="form-group">
+					<label for="url">Username:</label>
+					<input class="form-control" type="text" id="user" name="user">
+				</div>
+				<div class="form-group">
+					<label for="url">Password:</label>
+					<input class="form-control" type="text" id="pass" name="pass">
+				</div>
+			</div>
+			</div>
 		</div>
+	      </div>
+	</div>
+<?php
+/*
+
 		<div class="col-lg-12">
 			<h2>Test Result</h2>
 			<p>Test results are currently unavailable. Please first submit your code, then check back in a few minutes for results.</p>
@@ -67,7 +112,10 @@
 			<p><a class="btn btn-default" href="#" role="button">Check it out &raquo;</a></p>
 		</div>
 	</div>
-
+	* */
+?>
+	
+	</div>
 	<!-- jQuery Version 1.11.0 -->
 	<script src="js/jquery-1.11.0.js"></script>
 
