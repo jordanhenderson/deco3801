3ac2e964d11cc1f55e665115fee6cc0d1757a9b0
Addison <corrrosiveacid@gmail.com>
2014-09-02 18:01:28 +1000

    Done

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="index.php">Peer Code Review: <span style="color: white !important;"><?php echo $_SESSION['course_code'].': '.$_SESSION['course_title']; ?></span></a>
+				<a class="navbar-brand" href="index.php">Peer Code Review: <?php echo $_SESSION['course_code']; ?></a>
 			</div>
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
@@ -20,6 +20,7 @@
 							echo '<li><a href="help.php">Help Center</a></li>';
 						}
 					?>
+
 					<li><a href="reviewhub.php">Review Hub</a></li>
 					<li><a href="#">Feedback</a></li>
 				</ul>
