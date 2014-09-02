9b28ef155199b43fe7f40d656bca63546a0c085c
Addison <corrrosiveacid@gmail.com>
2014-09-02 17:48:49 +1000

    See previous
    Think this'll look good

--- header.php
+++ header.php
@@ -7,8 +7,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="index.php">Peer Code Review</a>
-				<span style="color: white !important;"><?php echo $_SESSION['course_code'].': '.$_SESSION['course_title']; ?></span>
+				<a class="navbar-brand" href="index.php">Peer Code Review: <span style="color: white !important;"><?php echo $_SESSION['course_code'].': '.$_SESSION['course_title']; ?></span></a>
 			</div>
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
