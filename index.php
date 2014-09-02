15b3d16cce5d28107dc3426109e7a2c1fda72b33
Addison <corrrosiveacid@gmail.com>
2014-09-02 17:00:06 +1000

    Now nobody can break it.

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $context = new BLTI('oF0jxF1IGjzxYUl9w8B', false, false);
 
 if ($context->valid) { // Redirect from Moodle, reload data, in case different course.
 	session_unset(); // clear old data, ready for reload from POST
-	$_SESSION['crs'] = new PCRHandler(); // new PCRHandler for session
+	$crs = $_SESSION['crs'] = new PCRHandler(); // new PCRHandler for session
 	$_SESSION['user_id'] = $_POST['user_id'];
 	$_SESSION['course_id'] = $_POST['context_id'];
 	$_SESSION['course_code'] = $_POST['context_label'];
@@ -21,12 +21,11 @@ if ($context->valid) { // Redirect from Moodle, reload data, in case different c
 	
 } else if (isset($_SESSION['user_id'])) {
 	// No action, since user is already authenticated, and data stored
+	$crs = $_SESSION['crs']; // just set this for easy referencing
 } else {
 	header('Location: invalid.php');
 	exit(); // User didn't come from Moodle, and isn't authenticated.
 }
-
-$crs = $_SESSION['crs']; // Stop moving this dammit, we need it for the first 2 conditions.
 
 ?>
 <!DOCTYPE html>
