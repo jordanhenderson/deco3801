5559d4a69a7212d72bb12da9a156933c25784fdf
Kshann <kieran_shannon11@hotmail.com>
2014-09-04 15:25:07 +1000

    Have it working, just not the w ay i want

--- storeQuestion.php
+++ storeQuestion.php
@@ -5,17 +5,17 @@ $title = $_POST['title'];
 $content = $_POST['content'];
 $fullname = $_SESSION['userfullname'];
 $stnid = $_SESSION['user_id'];
-//$question = new PCRHandler();
-//$question->getQuestion($_SESSION['course_id'])->addNewQuestion($title, $content, $stnid, //$fullname);
+$question = new PCRHandler();
+$question->getQuestion($_SESSION['course_id'])->addNewQuestion($title, $content, $stnid, //$fullname);
 //if($question!=null){
 //
 //}
 //local test
-$con = mysqli_connect("localhost","deco3801","hh2z2WG2q","deco3801") or
+/*$con = mysqli_connect("localhost","deco3801","hh2z2WG2q","deco3801") or
     die("Error:".mysqli_error($con));
--	$sql = "INSERT INTO `deco3801`.`Question` (`StudentID`, `CourseID`, `StudentName`, `Title`, `Content`, `Status`) VALUES ('".$stnid."', '2', '".$fullname."', '".$title."', '".$content."', '0');";
+-	$sql = "INSERT INTO `deco3801`.`Question` (`StudentID`, `CourseID`, `StudentName`, `Title`, `Content`, `Status`) VALUES ('2', '2', 'aname', 'atitle', 'aconten', '0');";
 
 -	$query = mysqli_query($con, $sql);
--	mysqli_close($con);
+-	mysqli_close($con);*/
 header('Location: help.php');
 ?>
