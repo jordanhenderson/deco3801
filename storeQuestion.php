0758050ad55cbf5a5bdb72ee632e1f1703fe9259
Kshann <kieran_shannon11@hotmail.com>
2014-09-04 15:41:05 +1000

    temp fix...

--- storeQuestion.php
+++ storeQuestion.php
@@ -6,15 +6,14 @@ $content = $_POST['content'];
 $fullname = $_SESSION['userfullname'];
 $stnid = $_SESSION['user_id'];
 $question = new PCRHandler();
-//$question->getQuestion($_SESSION['course_id'])->addNewQuestion($title, $content, $stnid, //$fullname);
+//$question->getQuestion($_SESSION['course_id'])->addNewQuestion($title, $content, $stnid, $fullname);
 //if($question!=null){
 //
 //}
-//local test
+//this is TEMPORARY FIX until i work out why the main thing isn't working....even though it was
 $con = mysqli_connect("localhost","deco3801","hh2z2WG2q","deco3801") or
     die("Error:".mysqli_error($con));
--	$sql = "INSERT INTO `deco3801`.`Question` (`StudentID`, `CourseID`, `StudentName`, `Title`, `Content`, `Status`) VALUES ('2', '2', 'aname', 'atitle', 'aconten', '0');";
-
+-	$sql = "INSERT INTO `deco3801`.`Question` (`StudentID`, `CourseID`, `StudentName`, `Title`, `Content`, `Status`) VALUES ('".$stnid."', '2', '".$fullname."', '".$title."', '".$content."', '0');";
 -	$query = mysqli_query($con, $sql);
 -	mysqli_close($con);
 header('Location: help.php');
