f2887f64cd1b3efbf982dd82b70793b9fc270c09
Sean <s4291495@student.uq.edu.au>
2014-09-04 17:04:20 +1000

    Testing code execution continued...

--- temp_testing/submit.php
+++ temp_testing/submit.php
@@ -1,14 +1,15 @@
 <?php
 
-//$out = array();
-//echo "Result: " . $result
-//foreach($out as $line) {
-    //echo $line;
-//}
+
 
 exec("tester.sh", $out, $result);
 
-echo "Result: " . $result;
-echo "Output: " . $out;
+echo "Result: " . $result . "\n";
+
+$out = array();
+echo "Result: " . $result
+foreach($out as $line) {
+    echo $line;
+}
 
 ?>
