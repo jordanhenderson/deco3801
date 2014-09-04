9c29110f84ceabd41cd56b6f42693d3a0c362457
Kshann <kieran_shannon11@hotmail.com>
2014-09-04 14:59:13 +1000

    Revert one of my changes

--- includes/db.php
+++ includes/db.php
@@ -375,10 +375,6 @@ class Question extends PCRObject {
 		$sth->execute(array($this->getID()));
 		
 	}	
-	public function testInsert(){
-		$sth = $this->db->prepare("INSERT INTO `deco3801`.`testTable` (`ID`) VALUES ('2');");
-		$sth->execute(array($this->getID()));
-	}
 
 	public function getQuestionContents($id){
 		$arr = array();
