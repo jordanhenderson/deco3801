4063f3c9a52fefab6f4c4985fc39aeae786c4dc3
Kshann <kieran_shannon11@hotmail.com>
2014-09-04 13:45:49 +1000

    hardcode for testing purposes

--- storeQuestion.php
+++ storeQuestion.php
@@ -7,9 +7,9 @@ $content = $_POST['content'];
 $fullname = $_SESSION['userfullname'];
 $stnid = $_SESSION['user_id'];
 $question = new PCRHandler();
-//$question->getQuestion($_SESSION['course_id'])->addNewQuestion($title, $content, $stnid, $fullname);
-//if($question!=null){
+$question->getQuestion(2)->addNewQuestion($title, $content, $stnid, $fullname);
+if($question!=null){
 header('Location: help.php');
-//}
+}
 
 ?>
