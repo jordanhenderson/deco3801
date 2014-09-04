17d0971d1b1c31c3472bf3edcce08b6866224540
Kshann <kieran_shannon11@hotmail.com>
2014-09-04 15:11:23 +1000

    testing

--- storeQuestion.php
+++ storeQuestion.php
@@ -5,9 +5,17 @@ $title = $_POST['title'];
 $content = $_POST['content'];
 $fullname = $_SESSION['userfullname'];
 $stnid = $_SESSION['user_id'];
-$question = new PCRHandler();
-$question->getQuestion($_SESSION['course_id'])->addNewQuestion($title, $content, $stnid, $fullname);
+//$question = new PCRHandler();
+//$question->getQuestion($_SESSION['course_id'])->addNewQuestion($title, $content, $stnid, //$fullname);
 //if($question!=null){
-//header('Location: help.php');
+//
 //}
+//local test
+$con = mysqli_connect("localhost","root","","deco3801") or
+    die("Error:".mysqli_error($con));
+-	$sql = "INSERT INTO `deco3801`.`question` (`QuestionID`, `StudentID`, `CourseID`, `StudentName`, `Opendate`, `Title`, `Content`, `Status`) VALUES (NULL, \'2\', \'2\', \'aname\', CURRENT_TIMESTAMP, \'atitle\', \'acontent\', \'0\');";
+
+-	$query = mysqli_query($con, $sql);
+-	mysqli_close($con);
+header('Location: help.php');
 ?>
