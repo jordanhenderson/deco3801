aca46b6bbe7212206dbe1008dcf6c541986c2cdd
Carlie Smits <carlie.smits@uqconnect.edu.au>
2014-09-04 14:06:03 +1000

    Token test
    Attempting to get tokens working for storing annotations

---
+++ includes/token.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once JWT.php;
+
+$CONSUMER_KEY = "30d9cf1909d34daabff333acea9412c0";
+$CONSUMER_SECRET = "bd9f20ea-d0da-4252-900e-9cc262b11be2";
+// Expire in 1 day
+$CONSUMER_TTL = 86400;
+
+$payload = array(
+	'consumerKey' => $CONSUMER_KEY,
+	'userId' => 'Admin',
+	'issuedAt' =>
+	'ttl' => $CONSUMER_TTL
+);
+
+$jwt = JWT::encode($payload, $CONSUMER_SECRET);
+echo $jwt;
+?>
