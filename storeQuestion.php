c6561f8ca1656f5dbacae5b538dfad556e7b9c80
Kshann <kieran_shannon11@hotmail.com>
2014-09-04 14:25:50 +1000

    please god

--- storeQuestion.php
+++ storeQuestion.php
@@ -6,7 +6,8 @@ $content = $_POST['content'];
 $fullname = $_SESSION['userfullname'];
 $stnid = $_SESSION['user_id'];
 $question = new PCRHandler();
-$question->getQuestion($_SESSION['course_id'])->addNewQuestion($title, $content, $stnid, $fullname);
+$question->getQuestion($_SESSION['course_id']);
+echo "got the question";//->addNewQuestion($title, $content, $stnid, $fullname);
 if($question!=null){
 header('Location: help.php');
 }
