954e0d91dbfe3d4a003b34e55cdc04f42fce7856
Carlie Smits <carlie.smits@uqconnect.edu.au>
2014-09-08 19:29:04 +1000

    Created development pages
    I've created some pages to try and get the store page working properly.
    I've left the other two for now however so there's definitely something
    to present on Friday

---
+++ load_dev.php
@@ -0,0 +1,17 @@
+<?php
+    	$filename = $_GET['filename'];
+	// Fix this up. May not need it at all because of handlers functions
+	$assignment = "/var/www/upload/course_00001/assign_00001/submissions/s1234567/" . $filename;
+		$handle = fopen($assignment, "r");
+		$contents = fread($handle, filesize($assignment));
+		echo "<code>" . $contents . "</code>";
+		fclose($handle);
+    /*$filename = $_GET['file'];
+    $assignment = "/var/www/upload/course_00001/assign_00001/submissions_00001/s1234567/" . $filename;
+    $handle = fopen($assignment, "r");
+    $contents = fread($handle, filesize($assignment));
+    $contents = str_replace('<', '&lt;', $contents);
+    $contents = str_replace('>', '&gt;', $contents);
+    echo $contents;
+    fclose($handle);*/
+?>
---
+++ review_dev.php
@@ -0,0 +1,123 @@
+<?php
+
+session_start();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Review - PCR</title>
+
+	<script src="js/jquery-1.11.0.js"></script>
+	
+	<!-- Bootstrap Core CSS -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Custom CSS -->
+	<link href="css/main.css" rel="stylesheet">
+
+	<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.1/styles/default.min.css">
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.1/highlight.min.js"></script>
+	<script>hljs.initHighlightingOnLoad();</script>
+	
+	<!-- Annotator -->
+	<script src="temp/annotator-full.min.js"></script> 
+	<link rel="stylesheet" type="text/css" href="temp/annotator.min.css">
+
+	<script>
+		
+		jQuery(function ($) {
+			$('#innercontainer').annotator();
+		});
+		/*Handles when someone clicks on the file tree*/
+		function handleSwap(id) {
+			$('a.active').removeClass('active');
+			$('#' + id.split('.')[0]).addClass('active');
+			
+			$.ajax({
+			  url: "load_dev.php?filename="+id,
+			  type: "POST"
+			})
+			  .done(function( filecode ) {
+				$( "#assignment_code" ).html( filecode );
+				$( "#file_heading" ).html( id );
+			});	  
+		}
+	</script>
+
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+	<?php include 'header.php'; ?>
+	
+	<div class="container">
+		<div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar">
+			<div class="list-group">
+			<?php
+			// Fix this so it's not hard coded
+		$dir = "/var/www/upload/course_00001/assign_00001/submissions/s1234567/";
+		$filesArray = array();
+		// Open a directory, and read its contents
+		if (is_dir($dir)){
+			if ($dh = opendir($dir)){
+				while (($file = readdir($dh)) !== false){
+					if ($file != "." && $file != ".."){
+						array_push($filesArray, $file);
+					}
+				}
+				closedir($dh);
+			}
+		}
+		foreach ($filesArray as $name) {
+			if ($name === $filesArray[0]) {
+				echo "<a href='#' id='" . explode('.', $name)[0] . "' class='list-group-item active' onclick='handleSwap(\"" . $name . "\");'>" . $name . "</a>";
+				continue;
+			}
+			echo "<a href='#' id='" . explode('.', $name)[0] . "' class='list-group-item' onclick='handleSwap(\"" . $name . "\");'>" . $name . "</a>";
+		}
+?>
+			</div>
+		</div>
+		<div class="col-md-9">
+			<h1>Assignment 99 Submission</h1>
+			<div class="col-md-12">
+				<h2 id="file_heading"></h2>
+				<div id="innercontainer">
+	<?php
+		if (count($filesArray) > 0) {
+			// Fix this so it's not hardcoded
+			$assignment = "/var/www/upload/course_00001/assign_00001/submissions/s1234567/" . $filesArray[0];
+			$handle = fopen($assignment, "r");
+			$contents = fread($handle, filesize($assignment));
+			echo "<pre id='assignment_code'><code>" . $contents . "</code></pre>";
+			fclose($handle);
+		}
+	?>			
+</div>	
+				<p>
+					<a class="btn btn-primary" href="reviewhub.php" role="button">Submit</a>
+					<a class="btn btn-info" href="#" role="button">Save</a>
+					<a class="btn btn-warning" href="#" role="button">Reset</a>
+					<a class="btn btn-warning" href="reviewhub.php" role="button">Close</a>
+				</p>
+			</div>
+		</div>
+	</div>
+	
+	<!-- jQuery Version 1.11.0 -->
+	<script src="js/jquery-1.11.0.js"></script>
+
+	<!-- Bootstrap Core JavaScript -->
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
