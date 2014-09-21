9b7426c910655f737b01408637fe54b9dcb4ae4f
Carlie Smits <carlie.smits@uqconnect.edu.au>
2014-09-22 00:49:13 +1000

    Test new way for storing 4

--- storeData_dev.php
+++ storeData_dev.php
@@ -7,28 +7,15 @@
     TODO: handle the submission ID and permissions
     */
     $reviews = json_decode($_GET['reviews']);
-    echo '<pre>';
-    print_r($reviews[0]);
-    echo '</pre>';
     $handler = new PCRHandler();
     $stnid = $_SESSION['user_id'];
     $id = $_SESSION['id'];
-    foreach ($reviews as $review) {
+    for ($i = 0; $i < count($review); $i++) {
         /* Id of reviewer, submission id, start position, start line, 
             end position, end line, comment contents, filename
         */
-        $handler->getReview($stnid, $id, $review[0], $review[1], $review[2],
-        $review[3], $review[4], $review[5]);
+        echo $review[$i]->comment . "%%";
+        $handler->getReview($stnid, $id, $review[$i]->startIndexSet, $review[$i]->startLine, $review[$i]->comment,
+        $review[$i]->text, $review[$i]->reviewID, $review[$i]->fileName);
     }
-    unset($review);
-    /*
-    $startIndex = $_GET['startIndex'];
-    $startLine = $_GET['startLine'];
-    $endIndex = $_GET['endIndex'];
-    $endLine = $_GET['i'];
-    $annotationText = $_GET['comment']; 
-    
-    
-    $review = new PCRHandler();
-    $review->getReview($stnid, $id, $annotationText, $anchor, $focus);*/
 ?>
