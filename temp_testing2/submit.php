66f44757a790b8da3628d0674e71895f74d21a86
0x534n <seanwinters13@hotmail.com>
2014-09-22 14:30:56 +1000

    Added debugging prints to testing process

--- temp_testing2/submit.php
+++ temp_testing2/submit.php
@@ -1,15 +1,20 @@
 <?php
+echo "starting submission process..." . PHP_EOL;
+
 error_reporting(E_ALL);
 
 require_once '../includes/testingAPI.php';
 
 //$tester = new functionalTestAPI();
 
+echo "executing script..." . PHP_EOL;
 $output = functionalTestAPI::executeBashScript("../../upload/tester.sh");
+
 foreach ($output as $value) {
 	echo $value . PHP_EOL;
 }
 
+echo "updating test restults..." . PHP_EOL;
 functionalTestAPI::dbUpdateTestResults(00001, $output);
 
 ?>
