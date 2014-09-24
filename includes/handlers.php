677a1b36049b47e965baa8cf309712442ee18c82
Kshann <kieran_shannon11@hotmail.com>
2014-09-25 03:03:28 +1000

    weird test, curious

--- includes/handlers.php
+++ includes/handlers.php
@@ -64,7 +64,11 @@ class PCRHandler {
 		$questionRow["Status"] = "0";
 		$question->commit();
 	}
-	
+	public function storeNewQuestion($title, $content, $stnid, $fullname){
+		$question = new Course(array("CourseID"=>$_SESSION['course_id']));
+		$question->addNewQuestion($title, $content, $stnid, $fullname);
+		$question->commit();
+	}
 	/**
 	 * getAssignment returns an assignment with the provided id.
 	 * @param id the assignment ID
