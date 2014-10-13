193f9c7eeae3d73f661c48349327d21149823676
0x534n <seanwinters13@hotmail.com>
2014-10-13 10:03:00 +1000

    Restructure TestingAPI

--- includes/testingAPI.php
+++ includes/testingAPI.php
@@ -3,102 +3,7 @@ error_reporting(E_ALL);
 
 require_once 'db.php';
 
-class functionalTestAPI {
-
-	/**
-	* This function will run the given bash script on the server, then return an 
-	* array of values indicating whether the test with that index succeeded 
-	* or not. 
-	* 
-	* i.e. if test 1 passes and test 2 fails,
-	* 		$testOutput[0] == "pass"
-	*		$testOutput[1] == "fail"
-	* 
-	* Script output format should be "test_number:pass/fail" without quotes, 
-	* separated by semi-colons. e.g. "1:pass;2:fail;3:pass"
-	* 
-	* Returns null if there were no test results
-	*/
-	public static function executeBashScript($script_path) {
-		// Execute student assignment
-		$scriptOutput = shell_exec($script_path);
-		
-		// Check for newline at the end, remove if present
-		if (substr($scriptOutput, -1) == "\n") {
-			$scriptOutput = substr($scriptOutput, 0, -1);
-		}
-
-		// Make sure the last char is not semi-colon for easier manipulation later
-		if (substr($scriptOutput, -1) == ";") {
-			$scriptOutput = substr($scriptOutput, 0, -1);
-		}
-
-		$testResults = explode(";", $scriptOutput);
-
-		if ($testResults == "" || $testResults == null) {
-			return null;
-		}
-
-		$counter = 0;
-
-		foreach ($testResults as $individualResult) {
-			$endResult = explode(":", $individualResult);
-
-			// TODO remove this after tests are created. For debugging purposes only
-			//echo "Test Number: " . $endResult[0] . " - " . $endResult[1] . PHP_EOL;
-
-			$counter++;
-			$testOutput[$counter] = $endResult[1];
-		}
-
-		return $testOutput;
-	}
-
-	/**
-	* This function takes a directory containing java source files and compiles them.
-	* It will return true if the compilation was successful, false otherwise.
-	* 
-	*/
-	public static function compileJavaSubmission($source_directory) {
-		$scriptOutput = shell_exec("javac " . $source_directory . "*.java");
-
-		echo "=== Compilation output ===\n" . $scriptOutput . "=======================\n";
-	}
-
-	/**
-	* This function takes a JUnit test file as input and runs it. If all tests
-	* pass, it will return true. Otherwise, it will return an array of test 
-	* names and results in the following format
-	*
-	* e.g. $failedTests[0] == "test_name:expected_result:actual_result"
-	*/
-	public static function runJUnitTestFile($test_class_path, $test_class) {
-		$scriptOutput = shell_exec("cd " . $test_class_path . " && java " . $test_class);
-
-		echo "=== Execution output ===\n" . $scriptOutput . "=======================\n";
-
-		if ($scriptOutput == "true") { // All tests passed
-			return true;
-		} else {
-			$counter = 0;
-
-			// Some tests failed, parse output to find names of failed tests
-			foreach(preg_split("/((\r?\n)|(\r\n?))/", $scriptOutput) as $line){
-    			if (strpos($line, 'expected') !== FALSE) {
-				    // Lines contained the string "expected" are lines with failing test names
-    				$testName = substr($line, 0, strpos($line, "("));
-    				$expectedResult = substr($line, strpos($line, "expected:") + 9, strpos($line, " but ") - (strpos($line, "expected:") + 9));
-    				$actualResult = substr($line, strpos($line, "was:") + 4);
-    				echo "Failed test: " . $testName . " -- Expected: " . $expectedResult . " -- Actual: " . $actualResult . PHP_EOL;
-
-    				$failedTests[$counter++] = $testName . ":" . $expectedResult . ":" . $actualResult;
-				}
-			}
-
-			return $failedTests;
-		}
-	}
-
+public class functionalTestAPI {
 	/**
 	* This function will update the given test results for the database submission entry.
 	* It will return true if the data is inserted successfully, false otherwise
@@ -142,13 +47,105 @@ class functionalTestAPI {
 			echo "Error retrieving test results from database\n";
 		}
 	}
+}
+
+public class javaTesting {
+	/**
+	* This function takes a directory containing java source files and compiles them.
+	* It will return true if the compilation was successful, false otherwise.
+	* 
+	*/
+	public static function compile($source_directory) {
+		$scriptOutput = shell_exec("javac " . $source_directory . "*.java");
+
+		echo "=== Compilation output ===\n" . $scriptOutput . "=======================\n";
+	}
 
 	/**
-	* This function will add the given script to the queue of files to be executed
-	* by the server
+	* This function takes a JUnit test file as input and runs it. If all tests
+	* pass, it will return true. Otherwise, it will return an array of test 
+	* names and results in the following format
+	*
+	* e.g. $failedTests[0] == "test_name:expected_result:actual_result"
 	*/
-	public static function addToQueue($script_path) {
+	public static function runJUnitTest($test_class_path, $test_class) {
+		$scriptOutput = shell_exec("cd " . $test_class_path . " && java " . $test_class);
 
+		echo "=== Execution output ===\n" . $scriptOutput . "=======================\n";
+
+		if ($scriptOutput == "true") { // All tests passed
+			return true;
+		} else {
+			$counter = 0;
+
+			// Some tests failed, parse output to find names of failed tests
+			foreach(preg_split("/((\r?\n)|(\r\n?))/", $scriptOutput) as $line){
+    			if (strpos($line, 'expected') !== FALSE) {
+				    // Lines contained the string "expected" are lines with failing test names
+    				$testName = substr($line, 0, strpos($line, "("));
+    				$expectedResult = substr($line, strpos($line, "expected:") + 9, strpos($line, " but ") - (strpos($line, "expected:") + 9));
+    				$actualResult = substr($line, strpos($line, "was:") + 4);
+    				echo "Failed test: " . $testName . " -- Expected: " . $expectedResult . " -- Actual: " . $actualResult . PHP_EOL;
+
+    				$failedTests[$counter++] = $testName . ":" . $expectedResult . ":" . $actualResult;
+				}
+			}
+
+			return $failedTests;
+		}
+	}
+}
+
+	
+
+public class bashTesting {
+	/**
+	* This function will run the given bash script on the server, then return an 
+	* array of values indicating whether the test with that index succeeded 
+	* or not. 
+	* 
+	* i.e. if test 1 passes and test 2 fails,
+	* 		$testOutput[0] == "pass"
+	*		$testOutput[1] == "fail"
+	* 
+	* Script output format should be "test_number:pass/fail" without quotes, 
+	* separated by semi-colons. e.g. "1:pass;2:fail;3:pass"
+	* 
+	* Returns null if there were no test results
+	*/
+	public static function execute($script_path) {
+		// Execute student assignment
+		$scriptOutput = shell_exec($script_path);
+		
+		// Check for newline at the end, remove if present
+		if (substr($scriptOutput, -1) == "\n") {
+			$scriptOutput = substr($scriptOutput, 0, -1);
+		}
+
+		// Make sure the last char is not semi-colon for easier manipulation later
+		if (substr($scriptOutput, -1) == ";") {
+			$scriptOutput = substr($scriptOutput, 0, -1);
+		}
+
+		$testResults = explode(";", $scriptOutput);
+
+		if ($testResults == "" || $testResults == null) {
+			return null;
+		}
+
+		$counter = 0;
+
+		foreach ($testResults as $individualResult) {
+			$endResult = explode(":", $individualResult);
+
+			// TODO remove this after tests are created. For debugging purposes only
+			//echo "Test Number: " . $endResult[0] . " - " . $endResult[1] . PHP_EOL;
+
+			$counter++;
+			$testOutput[$counter] = $endResult[1];
+		}
+
+		return $testOutput;
 	}
 }
 
