890b1108755d3e94b04358efffeb8c64548fe083
Sean <s4291495@student.uq.edu.au>
2014-10-16 23:42:49 +1000

    Uncomment redirect

--- upload.php
+++ upload.php
@@ -4,5 +4,5 @@ require_once('includes/handlers.php');
 $handler = new PCRHandler();
 $handler->uploadArchive($_POST["assignment_id"]);
 
-//header("Location: submit.php");
+header("Location: submit.php");
 ?>
