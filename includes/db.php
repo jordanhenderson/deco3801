989f5a546fdcd0275c3c295cad96d1152b341016
Sean <s4291495@student.uq.edu.au>
2014-10-18 00:27:13 +1000

    Removing debug code

--- includes/db.php
+++ includes/db.php
@@ -597,10 +597,6 @@ class Submission extends PCRObject {
 
 	public function testSubmission($assignment_type, $test_file_location) {
 		// Run appropriate tests
-		$this->row["Results"] = "Type:" . $assignment_type . " Loc:" . $test_file_location;
-		$this->commit();
-		return;
-
 		switch ($assignment_type) {
 			case 'bash':
 				// TODO remove hardcoding filename 
