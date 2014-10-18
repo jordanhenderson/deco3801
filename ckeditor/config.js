c5a64faf586a58ee99b11c8c822dfa1e691e4f6b
Kshann <kieran_shannon11@hotmail.com>
2014-10-18 23:00:06 +1000

    Removing a plugin

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -7,6 +7,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here.
 	// For complete reference see:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+	config.removePlugins = 'image,horizontalrule,table';
 	config.extraPlugins = 'codeTag';
 	// The toolbar groups arrangement, optimized for two toolbar rows.
 	config.toolbarGroups = [
@@ -23,7 +24,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
 	config.removeButtons = 'Underline,Subscript,Superscript, image,horizontalrule,table';
-
+	
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
 
