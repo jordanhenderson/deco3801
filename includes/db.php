febc1d0cacf8ecb95f1a9031ddc92f92d3937645
Kshann <Kieran@d-i89-241-3.student.eduroam.uq.edu.au>
2014-10-27 12:41:24 +1000

    Revert "order by claus... will revert if it messes up"
    This reverts commit bb6e84dce0952e044e05bc39f45ceb84f74ceab9.

--- includes/db.php
+++ includes/db.php
@@ -778,7 +778,7 @@ class Course extends PCRObject {
 	 */
 	public function getHelpCentreQuestions() {
 		$arr = array();
-		$sth = $this->db->prepare("SELECT * FROM Question WHERE CourseID = ? ORDER BY postdate DESC;");
+		$sth = $this->db->prepare("SELECT * FROM Question WHERE CourseID = ?;");
 		$sth->execute(array($this->getID()));
 		while ($file_row = $sth->fetch(PDO::FETCH_ASSOC)) {
 			array_push($arr, new Question($file_row));
